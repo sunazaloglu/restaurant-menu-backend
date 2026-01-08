fad21096777bcdd84f1e7dfbf465aaaf62cf8b61
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-08 18:55:16 +0300

    write create_products_table

--- src/migrations/20260108151110_create_products_table.js
+++ src/migrations/20260108151110_create_products_table.js
@@ -1,15 +1,20 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+export const up = async (knex) => {
+  await knex.schema.createTable("products", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("description").notNullable();
+    table.integer("price").notNullable();
+    table
+      .string("category_id")
+      .references("id")
+      .inTable("categories")
+      .onDelete("CASCADE");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").nullable();
+    table.timestamp("deleted_at").nullable();
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+export const down = async (knex) => {
+  await knex.schema.dropTable("products");
 };
