269b9fef5f04fbc71eecbe415d08296dce63a327
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-08 18:55:42 +0300

    write create_categories_table

--- src/migrations/20260108150956_create_categories_table.js
+++ src/migrations/20260108150956_create_categories_table.js
@@ -1,15 +1,14 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+export const up = async (knex) => {
+  await knex.schema.createTable("categories", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("description").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").nullable();
+    table.timestamp("deleted_at").nullable();
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+export const down = async (knex) => {
+  await knex.schema.dropTable("categories");
 };
