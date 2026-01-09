0d3490b42b5b35f0b066ceb3844713abf11e7436
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-09 15:31:05 +0300

    write categoryModel

---
+++ src/models/categoryModel.js
@@ -0,0 +1,11 @@
+import db from "../config/database.js";
+
+export const createCategory = async (category) => {
+  const [newItem] = await knex("categories").insert(category).returning("*");
+  return newItem;
+};
+
+export const getAllCategories = async () => {
+  const query = knex("categories").select("id", "name");
+  return query;
+};
