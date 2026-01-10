96016a5def622f8237502932ff8e0c8d1724fc6b
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-10 16:22:11 +0300

    import

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import categoryRoutes from "./routes/categoryRoutes.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
+app.use("/api/v1/categories", categoryRoutes);
 app.get("/projectcheck", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
