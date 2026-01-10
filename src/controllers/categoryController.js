5365556d38c324711404a7e732bdacd1c1cb6a0e
Su Naz Aloğlu <sualoglu@hotmail.com>
2026-01-10 16:24:18 +0300

    write categoryController

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -3,8 +3,8 @@ import { logger } from "../utils/logger.js";
 
 export const createItem = async (req, res) => {
   try {
-    const item = await req.body;
-    const newItem = createCategory(item);
+    const item = req.body;
+    const newItem = await createCategory(item);
     res.status(201).json(newItem);
   } catch (error) {
     logger(error, ERROR);
@@ -15,6 +15,7 @@ export const createItem = async (req, res) => {
 export const getAllItems = async (req, res) => {
   try {
     const items = await getAllCategories();
+    res.status(200).json(items);
   } catch (error) {
     logger(error, ERROR);
     res.status(500).json({ message: "Internal Server Error" });
